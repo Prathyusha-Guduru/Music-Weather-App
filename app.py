52f589398c1a334dc4b48b084002e28b1a5c9ffa
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-04-23 19:29:06 +0530

    Updating ajaz requests

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request
+import requests
 
 app = Flask(__name__)
 
@@ -8,6 +9,11 @@ def index():
 	print(status)
 	if(status == 1):
 		cords = request.form.get("coord", False)
+		print(f"Latitude is {cords[0]} and Longitude is {cords[1]}")
+		api_address = f"http://api.openweathermap.org/data/2.5/weather?lat={cords[0]}&lon={cords[1]}&appid=4f152fe40915295e05a97c1cdd65151d"
+		json_data = requests.get(api_address).json()
+		print(json_data["weather"][0]["description"])
+
 	else:
 		print("User denied geo acess")
 	return render_template('index.html')
--- static/script.js
+++ static/script.js
@@ -9,20 +9,22 @@ navigator.geolocation.getCurrentPosition((position)=>{
 
 function onSucess(lat,long){
 	console.log(`Latitude is ${lat} and longitude is ${long}`);
-	status = 1
+	let status = 1
 	$.post("/", {
 		userStatus : status,
 		coord : [lat,long]
 	});
+	console.log(`Status is ${status}`);
 	
 }
 
 function onFailure(message){
 	console.log(message);
-	status = 0
+	let status = 0
 	console.log(`if onfailure is getting executed ${status} and count is ${count}`);
 	$.post("/", {
 		userStatus : status,
 	});
+	console.log(`Status is ${status}`);
 }
 
