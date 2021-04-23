1590fdf21e012ddf1bf8651b5ffa1a1ebd12200a
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-04-23 18:52:07 +0530

    Updated geolocation api calls

--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
 // GEO-LOCATION API
+
+navigator.geolocation.getCurrentPosition((position)=>{
+	onSucess(position.coords.latitude,position.coords.longitude)
+},(positionError)=>{
+	onFailure(positionError.message)
+})
+
+function onSucess(lat,long){
+	console.log(`Latitude is ${lat} and longitude is ${long}`);
+}
+
+function onFailure(message){
+	console.log(message);
+}
