2a45e01256a6685beca6b9968a4e689ea93e4471
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-05-01 19:54:32 +0530

    Updated api calls

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
-from flask import Flask,render_template,request
+from flask import Flask, json,render_template,request,redirect,url_for
+from flask.signals import template_rendered
 import requests
 
-app = Flask(__name__)
-
-@app.route('/',methods = ['GET','POST'])
-def index():
-	status = request.form.get("userStatus", False)
-	print(f"status is {status} and its type is {type(status)}")
-	print(type(status))
+def temp_change(status):
 	if(status == '1'):
-		print(f"Status is {status} and yes user allowed")
 		lat = request.form.get("lat", False)
 		long = request.form.get("long", False)
 		print(f"{lat},{long}")
 		api_address = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid=4f152fe40915295e05a97c1cdd65151d"
 		json_data = requests.get(api_address).json()
 		print(json_data["weather"][0]["description"])
-
+		temp = json_data["main"]["temp"]
+		return redirect(url_for('weather'))
 	else:
-		print(f"Status is {status} and no, he did not allow")
-	return render_template('index.html')
+		temp = 0
+		return redirect(url_for('noaccess'))
+	return temp
+		
+
+app = Flask(__name__)
+@app.route('/',methods = ['GET','POST'])
+def index():
+	status = request.form.get("userStatus", False)
+
+	temp = temp_change(status)
+	lol = 302.7
+	i = 0
+	print(f"Temp is {temp_change(status)}")
+	return render_template('index.html',temp = temp)
+
+@app.route('/weather')
+def weather():
+	return render_template('weather.html')
+
+@app.route('/noaccess')
+def noaccess():
+	return render_template('noaccess.html')
 
 
 if __name__ == "__main__":
---
+++ trail.py
@@ -0,0 +1,8 @@
+
+temp = 0
+a = 5
+def trail():
+	global temp
+	temp = 45
+
+print(temp)
